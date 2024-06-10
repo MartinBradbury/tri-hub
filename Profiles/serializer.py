65a5f9c88e47419da33f1858d5c1de1c71686dae
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-10 20:32:43 +0000

    added filtering methods to profile model

--- Profiles/serializer.py
+++ Profiles/serializer.py
@@ -4,16 +4,31 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    post_count = serializers.ReadOnlyField()
+    follower_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
 
     class Meta:
         model = Profile
         fields = ['id', 'owner', 'created_at', 'updated_at',
                  'first_name', 'last_name',
                   'email', 'gender', 'fitness_level', 
-                  'image', 'content', 'is_owner',
+                  'image', 'content', 'is_owner', 'following_id',
+                  'follower_count', 'following_count',
                   ]
             
--- Profiles/views.py
+++ Profiles/views.py
@@ -1,15 +1,39 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, permissions, generics
+from rest_framework import status, permissions, generics, filters
 from .models import Profile
 from .serializer import ProfileSerializer
 from django.http import Http404
 from trihub.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        post_count = Count('owner__post', distinct=True),
+        # follower_count = Count('owner__followed', distinct=True),
+        # following_count = Count('owner__following', distinct=True),
+
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        # 'owner__following__followed__profile',
+        # 'owner__followed__owner__profile', # get all profiles that are followed by a profile, given its id
+    ]
+    
+    ordering_fields = [
+        'post_count',
+        # 'follower_count',
+        # 'following_count',
+        # 'owner__following__created_at',
+        # 'owner__followed__created_at',
+    ]
 
         
 
