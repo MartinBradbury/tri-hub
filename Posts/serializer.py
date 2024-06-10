aa67156cc7a80c5b9b944cbc02e638db7860b949
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-10 20:11:39 +0000

    migrated Likes model, imported into Posts serializer and view. Installed django-filter and filter options in Posts view

--- Posts/serializer.py
+++ Posts/serializer.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 from .models import Post
+from Likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    Like_id = serializers.SerializerMethodField()
+    Likes_count = serializers.ReadOnlyField()
+    
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_Like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -29,5 +42,6 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'owner', 'created_at', 'updated_at', 'title', 'content',
-                  'image', 'is_owner', 'profile_id', 'profile_image']
+                  'image', 'is_owner', 'profile_id', 'profile_image', 'Like_id',
+                  'Likes_count',]
             
--- Posts/views.py
+++ Posts/views.py
@@ -1,23 +1,79 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, permissions, generics
+from rest_framework import status, permissions, generics, filters
 from.models import Post
 from .serializer import PostSerializer
 from django.http import Http404
 from trihub.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PostList(generics.ListCreateAPIView):
-
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Post.objects.annotate(
+        Likes_count=Count('likes', distinct=True),
+        comment_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
 
+    filterset_fields = [
+        # 'owner__followed__owner__profile', #show post owner is following
+        'likes__owner__profile', #show posts that are liked by a specific user
+        'owner__profile' #show posts that are owned by a specific user
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comment_count',
+        'likes__created_at',
+        'comment__created_at',
+    ]
+    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-
     serializer_class = PostSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    permission_classes =[IsOwnerOrReadOnly]
+    queryset = Post.objects.annotate(
+        comment_count = Count('comment', distinct=True),
+        likes_count = Count('likes', distinct=True),
+    ).order_by('-created_at')
+
+
+
+
+
+
+
+
+
+
+
+
+# class PostList(generics.ListCreateAPIView):
+
+#     serializer_class = PostSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#     queryset = Post.objects.all()
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+
+#     serializer_class = PostSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+#     queryset = Post.objects.all()
