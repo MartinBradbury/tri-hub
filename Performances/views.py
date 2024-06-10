ce1a4db2dbd54b7009cf8597e762cee70ee93287
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-10 22:20:57 +0000

    added orfering filter to Performances

--- Performances/views.py
+++ Performances/views.py
@@ -1,13 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, permissions, generics
+from rest_framework import status, permissions, generics, filters
 from.models import UserPerformance, Event
 from .serializer import EventSerializer, PerformanceSerializer
 from django.http import Http404
 from trihub.permissions import IsOwnerOrReadOnly, SpecificUserFullAccess
-
-
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class EventListView(generics.ListCreateAPIView):
     serializer_class = EventSerializer
@@ -23,6 +21,17 @@ class PerformanceListView(generics.ListCreateAPIView):
     serializer_class = PerformanceSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = UserPerformance.objects.all()
+
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+
+    ordering_fields = [
+        'owner__username',
+        'event',
+        'time',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
