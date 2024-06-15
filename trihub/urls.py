f6e461f2c716ba294e5418670582119591d6ecea
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-15 13:45:25 +0000

    added registration url pathway

--- trihub/urls.py
+++ trihub/urls.py
@@ -32,4 +32,7 @@ urlpatterns = [
     path('', include('followers.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
 ]
