ee90e1b1b412934d7fe8b838e0a2afb475a5afce
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-16 16:31:39 +0000

    corrected typo in post serializer.

--- posts/serializer.py
+++ posts/serializer.py
@@ -2,47 +2,50 @@ from rest_framework import serializers
 from .models import Post
 from likes.models import Like
 
+
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    Like_id = serializers.SerializerMethodField()
-    Likes_count = serializers.ReadOnlyField()
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
     comment_count = serializers.ReadOnlyField()
-    
 
     def get_is_owner(self, obj):
-        request = self.context['request']
+        request = self.context["request"]
         return request.user == obj.owner
 
     def get_Like_id(self, obj):
-        user = self.context['request'].user
+        user = self.context["request"].user
         if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
-            ).first()
+            like = Like.objects.filter(owner=user, post=obj).first()
             return like.id if like else None
         return None
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError(
-                'Image size is too large, 2MB maximum.'
-            )
+            raise serializers.ValidationError("Image size is too large, 2MB maximum.")
         if value.image.width > 4096:
-            raise serializer.ValidationError(
-                'Image width is too large, 4096px maximum'
-            )
+            raise serializer.ValidationError("Image width is too large, 4096px maximum")
         if value.image.height > 4096:
-            raise serializer.ValidationError(
-                'Image height is too large, 4096 maximum'
-            )
+            raise serializer.ValidationError("Image height is too large, 4096 maximum")
         return value
 
     class Meta:
         model = Post
-        fields = ['id', 'owner', 'created_at', 'updated_at', 'title', 'content',
-                  'image', 'is_owner', 'profile_id', 'profile_image', 'Like_id',
-                  'Likes_count', 'comment_count',]
-            
+        fields = [
+            "id",
+            "owner",
+            "created_at",
+            "updated_at",
+            "title",
+            "content",
+            "image",
+            "is_owner",
+            "profile_id",
+            "profile_image",
+            "Like_id",
+            "Likes_count",
+            "comment_count",
+        ]
