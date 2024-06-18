fe1a9fc0f76b25dc7914dc81e50dbb65449ac1b8
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-18 20:24:11 +0000

    debut set to true to test error in front end

--- trihub/settings.py
+++ trihub/settings.py
@@ -59,7 +59,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+# 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
