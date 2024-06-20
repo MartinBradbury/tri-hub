3ba0a1f7bc662bd43babe0177894dd39f2970d82
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-20 08:10:23 +0000

    changed Cors policies

--- trihub/settings.py
+++ trihub/settings.py
@@ -117,9 +117,14 @@ MIDDLEWARE = [
 if "CLIENT_ORIGIN" in os.environ:
     CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
 
-CORS_ALLOWED_ORIGIN_REGEXES = [
-r'^https://.*\.codeinstitute-ide\.net$',
-]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r'^https://.*\.gitpod\.io$',
+    ]
+
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+# r'^https://.*\.codeinstitute-ide\.net$',
+# ]
 
 
 CORS_ALLOW_CREDENTIALS = True
