161db15840f25826cd7ba276ba37db5e8762f670
Martin Bradbury <martinbradbury@btinternet.com>
2024-07-01 23:27:06 +0000

    corrected indent error

--- trihub/permissions.py
+++ trihub/permissions.py
@@ -5,7 +5,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-    return obj.owner == request.user
+        return obj.owner == request.user
 
 
 """
