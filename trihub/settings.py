c50b6cc23c7ceab0869fc8a9081a342cf7bf7ceb
Martin Bradbury <martinbradbury@btinternet.com>
2024-07-02 19:37:17 +0000

    removed commented out in settings.

--- trihub/settings.py
+++ trihub/settings.py
@@ -23,7 +23,6 @@ CLOUDINARY_STORAGE = {
 }
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
